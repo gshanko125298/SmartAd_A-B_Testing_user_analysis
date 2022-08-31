747d4db177bbb87d3564b39ab430ea909929c59b
anteneh tilaye <antenehtilaye19@gmail.com>
2022-08-31 17:32:24 +0300

    adding helper functions

---
+++ scripts/HelperFunctions.py
@@ -0,0 +1,23 @@
+def hypergeom(k, K, n, N):
+    """Probability mass funciton of the hypergeometric distribution."""
+    return binom(K, k) * binom(N-K, n-k) / binom(N, n)
+
+
+def fisher_prob(m):
+    """Probability of a given observed contingency table according to Fisher's exact test."""
+    ((a, b), (c ,d)) = m
+    k = a
+    K = a+b
+    n = a+c
+    N = a+b+c+d
+    return hypergeom(k, K, n, N)
+
+def fisher_probs_histogram(m):
+    """Computes prob mass function histogram accroding to Fisher's exact test."""
+    neg_val = -min(m[0,0], m[1,1])
+    pos_val = min(m[1,0], m[1,0])
+    probs = []
+    for k in range(neg_val, pos_val+1):
+        m1 = m + np.array([[1, -1], [-1, 1]]) * k
+        probs.append(fisher_prob(m1))
+    return probs
